116fd03b600bb661bac554962c58a8547bc2472f
Maxime Moreillon <m.moreillon@gmail.com>
2023-11-25 08:05:49 +0900

    test routes

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 An Elysia.js middleware to export Prometheus metrics. Is is intended as a replacement to the [express prometheus bundle](https://www.npmjs.com/package/express-prom-bundle) for Elysia.
 
+## Installation
+
+```
+bun add elysia-prometheus-metrics
+```
+
 ## Usage
 
 ```typescript
 import { Elysia } from "elysia"
-import metricsMiddleware from "."
+import metricsMiddleware from "elysia-prometheus-metrics"
 
 const middlewareOptions = {}
 
--- server.ts
+++ server.ts
@@ -1,9 +1,20 @@
 import { Elysia } from "elysia"
 import metricsMiddleware from "."
+import { sleep } from "bun"
 
 const middlewareOptions = {}
 
 new Elysia()
   .use(metricsMiddleware(middlewareOptions))
-  .get("/", () => "Hello world")
+  .get("/", () => "Root")
+  .get("/greetings", () => "Hello world")
+  .post("/", () => "OK")
+  .get("/error", () => {
+    throw "You asked for it"
+  })
+  .get("/delay", async ({ query }) => {
+    const { duration = "100" } = query
+    await sleep(Number(duration))
+    return "Kept you waiting huh?"
+  })
   .listen(8080)
